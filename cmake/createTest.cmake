1fe076198b125032bc70a10e694f273dd071307f
Michael Allison <michaelallison@MALLISON.local>
2018-04-06 14:44:01 -0400

    cmake build working

--- cmake/createTest.cmake
+++ cmake/createTest.cmake
@@ -1,23 +1,15 @@
 cmake_minimum_required(VERSION 3.0)
 
-function(createTest TEST_NAME TEST_DIR)
-
-	message("Creating test: ${TEST_NAME} with locaiton: ${TEST_DIR}")
-
-	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/tests/${TEST_NAME}/${APP_TARGET})
-
-	file(GLOB TEST_FILES
-    	${TEST_DIR}/*.cpp
-	)
-
-	add_executable(${TEST_NAME} ${TEST_FILES})
-	target_include_directories(${TEST_NAME} PUBLIC ${TEST_DIR}/include)
-	target_link_libraries(${TEST_NAME} ${CATCH_LIB} ${MM_LIB})
-
-	set_target_properties (${TEST_NAME} PROPERTIES
-    	FOLDER tests
-    )
-
+function(createTest)
+	set(options)
+  	set(oneValueArgs NAME SOURCE LOCATION)
+  	set(multiValueArgs LIBS)
+  	cmake_parse_arguments(TEST "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+	message("Creating test: ${TEST_NAME}")
+	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/tests/${APP_TARGET})
+	file(GLOB ${TEST_NAME}_files ${TEST_SOURCE}/*.cpp)
+	add_executable(${TEST_NAME} ${${TEST_NAME}_files})
+	target_link_libraries(${TEST_NAME} ${TEST_LIBS})
 	catch_discover_tests(${TEST_NAME})
-
+	set_target_properties (${TEST_NAME} PROPERTIES FOLDER ${TEST_LOCATION})
 endFunction(createTest)
